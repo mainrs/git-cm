4a36f4683bc48792098e505ff501254114789ce7
Thomas Barusseau <6663654+tbarusseau@users.noreply.github.com>
2020-07-17 23:08:15 +0200

    fix: make issue footer-word human-readable (#14)
    This commit adds a footer word to related issues that matches the commit type. Issues related to a feature are prefixed with `Closes`. `fix` commits are prefixed with `Fixes`. A fallback value of `Referenced-issues` is used in all other cases.

--- src/git.rs
+++ src/git.rs
@@ -29,12 +29,32 @@ pub static DEFAULT_TYPES: Lazy<HashMap<&str, &str>> = Lazy::new(|| {
     m
 });
 
+fn format_footer(commit_type: &str, issues_list: &[String]) -> String {
+    let footer_key = match commit_type {
+        "fix" => "Fixes",
+        "feat" => "Closes",
+        _ => "Referenced-issues",
+    };
+    let mut footer_value = String::new();
+    issues_list.iter().for_each(|s| {
+        footer_value.push_str(s);
+        footer_value.push(' ')
+    });
+
+    let footer_separator = match footer_value.chars().next() {
+        Some('#') => " ",
+        _ => ": ",
+    };
+
+    format!("{}{}{}", footer_key, footer_separator, footer_value)
+}
+
 /// Generates the commit message by selectively appending all parts that the
 /// user entered.
 pub fn generate_commit_msg(survey: SurveyResults) -> String {
     let commit_type_and_scope = match survey.scope {
         Some(scope) => format!("{}({})", survey.commit_type, scope),
-        None => survey.commit_type,
+        None => survey.commit_type.to_owned(),
     };
     let pre_colon = match survey.breaking_changes_desc {
         Some(_) => format!("{}!", commit_type_and_scope),
@@ -50,7 +70,11 @@ pub fn generate_commit_msg(survey: SurveyResults) -> String {
         None => with_long_msg,
     };
     match survey.affected_open_issues {
-        Some(issue_list) => format!("{}\n\n{}", with_breaking_change, issue_list),
+        Some(issues_list) => format!(
+            "{}\n\n{}",
+            with_breaking_change,
+            format_footer(&survey.commit_type, &issues_list)
+        ),
         None => with_breaking_change,
     }
 }
