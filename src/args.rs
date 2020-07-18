ffa1ca0c88239cf23a5157030f90502d09f35541
Sven <SirWindfield@users.noreply.github.com>
2020-07-18 16:52:19 +0200

    feat: add editor mode (#25)
    The new command line flag allows users to open up their default editor instead of going through the dialog process. A conventional-commit friendly template is displayed.
    
    Closes #18

--- src/args.rs
+++ src/args.rs
@@ -4,6 +4,8 @@ use std::path::PathBuf;
 #[derive(Clap, Debug)]
 #[clap(author, version)]
 pub struct App {
+    #[clap(long, short = "e")]
+    pub edit: bool,
     #[clap(name = "REPO", default_value = ".", parse(from_os_str))]
     pub repo_path: PathBuf,
 }
