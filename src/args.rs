dd49427d44d278fe9560015ce110dfee52085cf9
Sven <SirWindfield@users.noreply.github.com>
2020-07-18 16:58:25 +0200

    feat(cli): add help description for flags (#26)

--- src/args.rs
+++ src/args.rs
@@ -4,8 +4,11 @@ use std::path::PathBuf;
 #[derive(Clap, Debug)]
 #[clap(author, version)]
 pub struct App {
+    /// Opens the user's editor to type the commit message instead of showing a
+    /// dialog.
     #[clap(long, short = "e")]
     pub edit: bool,
+    /// The path to a git repository.
     #[clap(name = "REPO", default_value = ".", parse(from_os_str))]
     pub repo_path: PathBuf,
 }
